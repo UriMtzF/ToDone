9f200b75d711c317d7d2d7bdee7b85e323093edc
UriMtzF <39018208+UriMtzF@users.noreply.github.com>
2023-09-17 20:38:06 -0600

    Maded read method static

--- src/main/java/com/todone/tasks/RWFile.java
+++ src/main/java/com/todone/tasks/RWFile.java
@@ -1,32 +1,18 @@
-package com.todone.parser;
+package com.todone.tasks;
 
 import java.io.*;
 
 public class RWFile {
-    private final String FILE_PATH;
-    private StringBuilder content;
-
-    public RWFile(String filePath) {
-        this.FILE_PATH = filePath;
-    }
-
-    public StringBuilder getContent() {
-        return content;
-    }
-
-    public void setContent(StringBuilder content) {
-        this.content = content;
-    }
-
-    private void readContent() {
+    public static StringBuilder readFileContent(String filePath) {
+        StringBuilder content = new StringBuilder();
         try {
-            FileReader fileReader = new FileReader(this.FILE_PATH);
+            FileReader fileReader = new FileReader(filePath);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             String line;
 
             while ((line = bufferedReader.readLine()) != null){
-                this.content.append(line).append("\n");
+                content.append(line).append("\n");
             }
 
             bufferedReader.close();
@@ -35,5 +21,6 @@ public class RWFile {
         } catch (IOException e) {
             System.out.println("Ocurrio un error al leer el archivo" + e.getMessage());
         }
+        return content;
     }
 }
